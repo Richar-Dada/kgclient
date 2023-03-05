b27dc55aa661ba836c88aaffe24e000d5e94136c
Richarlu <luzicong@realibox.com>
2023-03-05 21:21:56 +0800

    Revert "feat: 屏蔽发票支付"
    This reverts commit ac88b4d598f1600ac77e0c49f8bf0eef43bc7e97.

--- pages/invoiceRecord/index.vue
+++ pages/invoiceRecord/index.vue
@@ -1044,18 +1044,13 @@
 					if (res.code === 200) {
 						uni.showToast({title:"登记成功", icon:"success"});
 						uni.$emit('inoviceCreate')
-						// 暂时屏蔽
-						// setTimeout(() => {
-						// 	const payPrice = this.formData.carId.indexOf('粤A') > -1 ? 50 : 150
-						// 	console.log('dd', this.formData.carId, this.formData.carId.indexOf('粤A') > -1)
-						// 	uni.redirectTo({
-						// 		url: '../payment/index?pid=' + res.data.pid + '&payPrice=' +  payPrice
-						// 	})
-						// }, 1000)
-						
-						uni.redirectTo({
-							url: '../invoiceList/index'
-						})
+						setTimeout(() => {
+							const payPrice = this.formData.carId.indexOf('粤A') > -1 ? 50 : 150
+							console.log('dd', this.formData.carId, this.formData.carId.indexOf('粤A') > -1)
+							uni.redirectTo({
+								url: '../payment/index?pid=' + res.data.pid + '&payPrice=' +  payPrice
+							})
+						}, 1000)
 					} else {
 						uni.showToast({title: res.msg, icon:"none"});
 					}
